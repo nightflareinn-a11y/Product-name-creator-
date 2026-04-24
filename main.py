65726d4e7d55f2983040c9db9306f0b9be910109
nightflareinn-a11y <nightflare.inn@gmail.com>
2026-04-24 15:27:43 +0530

    Add product name creator script

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is a Product Name Creator
+brand_name = input("Enter the brand name: ")
+model_name = input("Enter the model name: ")
+product_name = brand_name + " " + model_name
+print(product_name)
